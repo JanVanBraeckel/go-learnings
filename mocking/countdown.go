ba265e10705fb3d337ac35b2f284f3ce4625e0bc
Jan Van Braeckel <janvanbraeckel1995@gmail.com>
2021-10-25 22:11:14 +0200

    Mocking

---
+++ mocking/countdown.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (s *ConfigurableSleeper) Sleep() {
+	s.sleep(s.duration)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+
+	sleeper.Sleep()
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	Countdown(os.Stdout, &ConfigurableSleeper{duration: 1 * time.Second, sleep: time.Sleep})
+}
